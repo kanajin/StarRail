37c350985e4625c2294ffcf817ae75bb10839eeb
Suzuki <ekanajin@gmail.com>
2023-06-02 21:56:39 +0800

    新增team类

---
+++ team.py
@@ -0,0 +1,28 @@
+from chara import Character, character_factory
+
+
+def team_factory():
+    chara_list = character_factory()
+
+    if len(chara_list) > 4:
+        raise ValueError("too long")
+
+    team = Team()
+    chara_set = {}
+
+    for v in chara_list:
+        if isinstance(v, Character):
+            chara_set.clear()
+            raise TypeError("input character")
+        chara_set.add(v)
+
+    team.chara_set = chara_set
+    # TODO:未来若加入遗器套装效果，则需要修改此部分代码
+    team.cost = 3
+
+    return team
+
+
+class Team():
+    chara_set: set(Character)
+    cost: int
